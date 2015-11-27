cb741d6633d71bc604a87aef3759e6335648b989
albertohg <alberto.heredia@live.u-tad.com>
2015-11-27 20:03:15 +0100

    Cambio del nombre del paquete

--- parchis/src/parchis/logic/Parchis.java
+++ parchis/src/parchis/logic/Parchis.java
@@ -1,8 +1,9 @@
-package parchis;
+package parchis.logic;
 
 public class Parchis {
 
 	public static void main(String[] args) {
+		System.out.println("¡Bienvenido al juego del parchís!");
 		System.out.println("¡Bienvenido al juego del parchís!");
 	}
 
