169e10ad47cad84c2c6b86421d8ab543ec3cdddc
aheredia <alberto.heredia@live.u-tad.com>
2015-12-15 19:58:58 +0100

    Eliminada línea duplicada

--- parchis/src/parchis/logic/Parchis.java
+++ parchis/src/parchis/logic/Parchis.java
@@ -3,8 +3,6 @@ package parchis.logic;
 public class Parchis {
 
 	public static void main(String[] args) {
-		System.out.println("¡Bienvenido al juego del parchís!");
-		System.out.println("¡Bienvenido al juego del parchís!");
+		System.out.println("¡Disfruta jugando al parchís!");
 	}
-
 }
